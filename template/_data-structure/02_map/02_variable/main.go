0c699726ce1e5a142b75ed0ae11263df95cf5a3b
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-11-07 06:36:42 -0800

    using map to pass data to template with keys

---
+++ template/_data-structure/02_map/02_variable/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+func main() {
+	data := map[string]string{
+		"India":    "Gandhi",
+		"America":  "MLK",
+		"Meditate": "Buddha",
+		"Love":     "Jesus",
+		"Prophet":  "Muhamed",
+	}
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
